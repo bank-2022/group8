a91aca4c50ce591698b848d6d58809dc6641d832
nismq <jereniss.00@gmail.com>
2022-04-08 11:59:20 +0300

    päivitetty

--- api/app.js
+++ api/app.js
@@ -6,14 +6,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const userRouter = require('./routes/user_table');
-<<<<<<< HEAD
 const transactionsRouter = require('./routes/transactions_table');
-=======
 const cardRouter = require('./routes/card_table');
 const accountRouter = require('./routes/account_table');
 const loginRouter = require('./routes/login');
 
->>>>>>> 0bcaf6fdad0333225dc5c9e70cdfe08107900d20
 var app = express();
 
 const helmet = require('helmet');
@@ -31,13 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/user', userRouter);
-<<<<<<< HEAD
 app.use('/transactions' , transactionsRouter);
-=======
 app.use('/card', cardRouter);
 app.use('/', accountRouter);
 app.use('/login', loginRouter);
 
 
->>>>>>> 0bcaf6fdad0333225dc5c9e70cdfe08107900d20
 module.exports = app;
